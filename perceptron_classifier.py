6d7ab7e081a0cbbd82d0de84090ab50d93f94e75
Basem Rizk <basem.rizk@outlook.com>
2019-11-25 16:29:53 +0200

    perceptron training optimized + update weights + calculating confusion matrices

--- perceptron_classifier.py
+++ perceptron_classifier.py
@@ -93,32 +93,37 @@ def train_perceptron(X_train, T_train, learning_rate):
     weights_updated = True
     for epoch in range(500):
         print_progress("@ Epoch", epoch)
-        if not(weights_updated):
-            break
         weights_updated = False
         for row in range(X_train.shape[0]):
             
-            x_sample = np.expand_dims(X_train[row],axis = 1)
+            x_sample = X_train[row]
             t_sample = T_train[row]
             
             predicted_label =\
-                activation_function(np.mat(weights.T)*np.mat(x_sample))
+                activation_function(weights.T.dot(x_sample))
                 
             if(predicted_label != t_sample):
                 weights = weights +\
-                    (learning_rate*np.mat(x_sample)*t_sample)
+                    (learning_rate*x_sample*t_sample)
                 weights_updated = True
+                
+        if not(weights_updated):
+            break
+        
     print()
     print("Finished training.")
     return weights
 
-def perceptron_predict(X_test, weights):
-    X_test_expanded = np.expand_dims(X_test, axis = 2)
+def perceptron_predict(X_test, weights, one_versus_one = False):
     predictions = np.zeros(X_test.shape[0])
-    for i in range(X_test_expanded.shape[0]):
-        x_sample = X_test_expanded[i]
-        predictions[i] =\
-            activation_function(np.mat(weights.T)*np.mat(x_sample))
+    for i in range(X_test.shape[0]):
+        x_sample = X_test[i]
+        if(one_versus_one):
+            predictions[i] =\
+                activation_function(weights.T.dot(x_sample))
+        else:
+            predictions[i] =\
+                weights.T.dot(x_sample)
     return predictions
 
 # =============================================================================
@@ -126,17 +131,20 @@ def perceptron_predict(X_test, weights):
 # =============================================================================
 X_train, T_train_labels = get_data(train_path, train_labels_path, 2400)
 X_test, T_test_labels = get_data(test_path, test_labels_path, 200)
+X_train = np.expand_dims(X_train, axis = 2)
+X_test = np.expand_dims(X_test, axis = 2)
 
 # Training Phase
 all_weights = np.zeros((10, 10, X_train.shape[1]))
 for i in range(0, 10):
     print("=> Begin training on handwritten digit '" + str(i) + "'")
+    T_train =  np.where(T_train_labels == i, 1, -1)
     for learning_phase in range(0, 10):
         learning_rate = math.pow(10, -learning_phase)
         print("..with learning_rate = " + str(learning_rate))
-        T_train =  np.where(T_train_labels == i, 1, -1)
         weights = train_perceptron(X_train, T_train, learning_rate)
-        all_weights[i][learning_phase] = weights.reshape(weights.shape[0])[0]
+        all_weights[i][learning_phase] = weights.reshape(weights.shape[0])
+
 
 # Saving trained weights
 if not(os.path.exists(weights_dir_path)):    
@@ -159,11 +167,11 @@ for i in range(all_weights.shape[0]):
         all_predictions[j][i] = predictions
 
 
-# TODO Calculating confusion matrices
+# Calculating confusion matrices
 if not(os.path.exists(confusion_dir_path)):
     os.makedirs(confusion_dir_path)
     for i in range(0, 10):
-        confusion_matrix = create_confusion_matrix(all_predictions[i], T_test_labels)
+        confusion_matrix = create_confusion_matrix(all_predictions[i].T, T_test_labels)
         img_filepath = os.path.join(confusion_dir_path,
                                     "Confusion-" + str(i) + ".jpg")
         save_and_scale_confusion_matrix(confusion_matrix, img_filepath)
